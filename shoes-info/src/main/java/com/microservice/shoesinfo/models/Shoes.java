8d497669eb5a1ec9c5dff9f8bb81aef3e9ee8cec
gabrielbiscaia <ra118928@uem.br>
2024-01-05 18:28:33 -0300

    utilização das novas bibliotecas na classe shoes

--- shoes-info/src/main/java/com/microservice/shoesinfo/models/Shoes.java
+++ shoes-info/src/main/java/com/microservice/shoesinfo/models/Shoes.java
@@ -1,37 +1,23 @@
 package com.microservice.shoesinfo.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Shoes {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private String id;
     private String name;
     private String description;
-    public Shoes(String id, String name, String description) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-    }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
 }
