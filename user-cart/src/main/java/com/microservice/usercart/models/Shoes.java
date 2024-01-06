cea87c1ffa1fc2eeade6ee2569f2a4ea06a7f99a
gabrielbiscaia <ra118928@uem.br>
2024-01-05 23:19:15 -0300

    atualização da entidade shoes para ficar similar a do outro microsserviço

--- user-cart/src/main/java/com/microservice/usercart/models/Shoes.java
+++ user-cart/src/main/java/com/microservice/usercart/models/Shoes.java
@@ -1,37 +1,18 @@
 package com.microservice.usercart.models;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Shoes {
 
-    private String id;
-    private String name;
-    private String description;
-    public Shoes(String id, String name, String description) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-    }
+    Long id;
+    String name;
+    String description;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
 }
-
