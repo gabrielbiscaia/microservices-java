018709498ddc400e3456c724e78efe564525b417
gabrielbiscaia <ra118928@uem.br>
2024-01-05 23:19:48 -0300

    Atualização do CartItem com uso do Lombok

--- user-cart/src/main/java/com/microservice/usercart/models/CartItem.java
+++ user-cart/src/main/java/com/microservice/usercart/models/CartItem.java
@@ -1,28 +1,18 @@
 package com.microservice.usercart.models;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CartItem {
 
     private String name;
     private String description;
 
-    public CartItem(String name, String description) {
-        this.name = name;
-        this.description = description;
-    }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
